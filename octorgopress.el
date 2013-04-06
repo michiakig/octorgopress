e46e4073e4e10f6f0734529c6495e4e892ef01b3
aki <spacemanaki@gmail.com>
2013-04-05 22:58:24 -0400

    rename from Markdown backend to Octopress backend

--- octorgopress.el
+++ octorgopress.el
@@ -1,22 +1,22 @@
-;; Markdown backend for Org-mode
+;; Octopress backend for Org-mode
 ;; Depends on latest (bleeding development branch, maybe v8.x) of Org
 ;; uses generic export: http://orgmode.org/worg/dev/org-export-reference.html
 
 (require 'ox)
 
-(org-export-define-backend 'markdown
+(org-export-define-backend 'octopress
   '(
-    (bold . org-markdown-bold)
-    (headline . org-markdown-headline)
-    (italic . org-markdown-italic)
-    (link . org-markdown-link)
-    (paragraph . org-markdown-paragraph)
-    (section . org-markdown-section)
-    (src-block . org-markdown-src-block)
-    (template . org-markdown-template)
+    (bold . org-octopress-bold)
+    (headline . org-octopress-headline)
+    (italic . org-octopress-italic)
+    (link . org-octopress-link)
+    (paragraph . org-octopress-paragraph)
+    (section . org-octopress-section)
+    (src-block . org-octopress-src-block)
+    (template . org-octopress-template)
 ))
 
-(defun org-markdown-template (contents info)
+(defun org-octopress-template (contents info)
   "Accepts the final transcoded string and a plist of export options,
 returns final string with YAML frontmatter as preamble"
   (let ((title (plist-get info :title))
@@ -35,7 +35,7 @@ categories:
     (concat (format frontmatter title date time)
             contents)))
 
-(defun org-markdown-src-block (src-block contents info)
+(defun org-octopress-src-block (src-block contents info)
   "Transcode a #+begin_src block from Org to Github style backtick code blocks"
   (let ((lang (org-element-property :language src-block))
         (value (org-element-property :value src-block))
@@ -51,7 +51,7 @@ categories:
     (dotimes (_ n acc)
       (push x acc))))
 
-(defun org-markdown-headline (headline contents info)
+(defun org-octopress-headline (headline contents info)
   (let ((value (org-element-property :raw-value headline))
         (level (org-element-property :level headline)))
     (concat (apply 'concat (repeat "#" level))
@@ -60,41 +60,41 @@ categories:
             "\n"
             contents)))
 
-(defun org-markdown-link (link contents info)
+(defun org-octopress-link (link contents info)
   (let ((path (org-element-property :raw-link link)))
     (format "[%s](%s)" contents path)))
 
-(defun org-markdown-paragraph (paragraph contents info)
+(defun org-octopress-paragraph (paragraph contents info)
   contents)
 
-(defun org-markdown-section (section contents info)
+(defun org-octopress-section (section contents info)
   contents)
 
-(defun org-markdown-italic (elt contents info)
-  "Transcode italic text to Markdown equiv of <em>"
+(defun org-octopress-italic (elt contents info)
+  "Transcode italic text to Octopress equiv of <em>"
   (format "*%s*" contents))
 
-(defun org-markdown-bold (text contents info)
-  "Transcode bold text to Markdown equiv of <strong>"
+(defun org-octopress-bold (text contents info)
+  "Transcode bold text to Octopress equiv of <strong>"
   (format "**%s**" contents))
 
-(defun org-markdown-export-as-markdown
+(defun org-octopress-export-as-octopress
   (&optional async subtreep visible-only body-only ext-plist)
   (interactive)
   (if async
       (org-export-async-start
           (lambda (output)
-            (with-current-buffer (get-buffer-create "*Org Markdown Export*")
+            (with-current-buffer (get-buffer-create "*Org Octopress Export*")
               (erase-buffer)
               (insert output)
               (goto-char (point-min))
-              (org-export-add-to-stack (current-buffer) 'markdown)))
-        `(org-export-as 'markdown ,subtreep ,visible-only ,body-only ',ext-plist))
-    (let ((outbuf (org-export-to-buffer 'markdown "*Org Markdown Export*"
+              (org-export-add-to-stack (current-buffer) 'octopress)))
+        `(org-export-as 'octopress ,subtreep ,visible-only ,body-only ',ext-plist))
+    (let ((outbuf (org-export-to-buffer 'octopress "*Org Octopress Export*"
                                         subtreep visible-only body-only ext-plist)))
       (with-current-buffer outbuf (LaTeX-mode))
       (when org-export-show-temporary-export-buffer
         (switch-to-buffer-other-window outbuf)))))
 
-(defun org-markdown-publish-to-markdown (plist filename pub-dir)
-  (org-publish-org-to 'markdown filename ".markdown" plist pub-dir))
+(defun org-octopress-publish-to-octopress (plist filename pub-dir)
+  (org-publish-org-to 'octopress filename ".md" plist pub-dir))
